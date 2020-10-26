b4a50b4f0eb2da1fb73707e7827efd7b07fef0a3
bb s <424626154@qq.com>
2020-10-27 06:20:43 +0800

    flutter plugin

---
+++ lib/gdt/gdt_interstitial.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/services.dart';
+import 'flutterGdtPlugin.dart';
+
+typedef UnifiedInterstitialAdEventCallback = Function(UnifiedInterstitialAdEvent event, dynamic arguments);
+enum UnifiedInterstitialAdEvent {
+  onNoAd,
+  onAdReceived,
+  onAdExposure,
+  onAdClosed,
+  onAdClicked,
+  onAdLeftApplication,
+  onAdOpened,
+}
+
+class GDTInterstitial{
+  final String posId;
+
+  final UnifiedInterstitialAdEventCallback adEventCallback;
+
+  MethodChannel _methodChannel;
+
+  GDTInterstitial(this.posId, {this.adEventCallback}) {
+    var channelName = 'plugins.hetian.me/gdt_plugins/interstitial/$posId';
+    print('$channelName');
+    this._methodChannel = MethodChannel(channelName);
+    this._methodChannel.setMethodCallHandler(_handleMethodCall);
+    FlutterGdtPlugin.createInterstitialAd(posId: posId);
+  }
+
+  Future<void> _handleMethodCall(MethodCall call) async {
+    if(adEventCallback != null) {
+      UnifiedInterstitialAdEvent event;
+      switch (call.method) {
+        case 'onNoAd':
+          event = UnifiedInterstitialAdEvent.onNoAd;
+          break;
+        case 'onAdReceived':
+          event = UnifiedInterstitialAdEvent.onAdReceived;
+          break;
+        case 'onAdExposure':
+          event = UnifiedInterstitialAdEvent.onAdExposure;
+          break;
+        case 'onAdClosed':
+          event = UnifiedInterstitialAdEvent.onAdClosed;
+          break;
+        case 'onAdClicked':
+          event = UnifiedInterstitialAdEvent.onAdClicked;
+          break;
+        case 'onAdLeftApplication':
+          event = UnifiedInterstitialAdEvent.onAdLeftApplication;
+          break;
+        case 'onAdOpened':
+          event = UnifiedInterstitialAdEvent.onAdOpened;
+          break;
+      }
+      adEventCallback(event, call.arguments);
+    }
+  }
+
+  Future<void> loadAd() async {
+    await _methodChannel.invokeMethod('load');
+  }
+
+  Future<void> closeAd() async {
+    await _methodChannel.invokeMethod('close');
+  }
+
+  Future<void> showAd() async {
+    await _methodChannel.invokeMethod('show');
+  }
+
+  Future<void> showAdAsPopup() async {
+    await _methodChannel.invokeMethod('popup');
+  }
+
+}
---
+++ lib/gdt_plugin.dart
@@ -0,0 +1,4 @@
+export 'gdt/flutterGdtPlugin.dart';
+export 'gdt/listeners.dart';
+export 'gdt/views.dart';
+export 'gdt/gdt_interstitial.dart';
